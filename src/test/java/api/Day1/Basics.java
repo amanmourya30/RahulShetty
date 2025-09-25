d7241e9f9d2cbd6e86429166097bb0bd160a83da
aman_xten <aman@xtenav.com>
2025-09-25 19:12:55 +0530

    first commit

---
+++ src/test/java/api/Day1/Basics.java
@@ -0,0 +1,18 @@
+package api.Day1;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
+public class Basics {
+	
+	@Test
+	void basicTest()
+	{
+		given()
+		.when()
+		.then();
+	}
+
+}
