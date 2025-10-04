87c7e4b33aa9c72de0da9864a68d8308243b0569
aman_xten <aman@xtenav.com>
2025-10-04 18:18:34 +0530

    completed ddtexcel with maven in pom

--- src/test/java/dataDrivenExcelAndJSON/DDTexcel.java
+++ src/test/java/dataDrivenExcelAndJSON/DDTexcel.java
@@ -6,14 +6,15 @@ import static org.hamcrest.Matchers.equalTo;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import org.testng.Assert;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import pojo.Address;
 import pojo.User;
@@ -21,10 +22,32 @@ import utilities.ExcelUtils;
 
 public class DDTexcel {
 	private static List<String> createdIds= new ArrayList<String>();
+	private static RequestSpecification requestSpec;
+	private static ResponseSpecification responseSpec200;
+	private static ResponseSpecification responseSpec201;
+	private static ResponseSpecification responseSpec404;
+	private static ResponseSpecification responseSpec200or204;
 	
 	@BeforeClass
 	void setup() {
-	    RestAssured.baseURI = "http://localhost:3000";
+	    
+	    requestSpec = new RequestSpecBuilder()
+	    			.setBaseUri("http://localhost:3000")
+	    			.setContentType(ContentType.JSON)
+	    			.build();
+	    responseSpec201 = new ResponseSpecBuilder()
+					.expectStatusCode(201)
+					.build();
+	    responseSpec200 = new ResponseSpecBuilder()
+					.expectStatusCode(200)
+					.build();
+	    responseSpec404 = new ResponseSpecBuilder()
+					.expectStatusCode(404)
+					.build();
+	    responseSpec200or204 = new ResponseSpecBuilder()
+	            .expectStatusCode(anyOf(equalTo(200), equalTo(204)))
+	            .build();
+
 	}
 	
 	@Test(dataProvider="ExcelUserData" ,priority = 1)
@@ -32,28 +55,14 @@ public class DDTexcel {
             String is_active, String street, String city, String zipcode,
             String skills) {
 		// --- Build POJO ---
-	    User user = new User();
-	    user.setId(id);
-	    user.setEmail(email);
-	    user.setFirst_name(first_name);
-	    user.setLast_name(last_name);
-	    user.setIs_active(Boolean.parseBoolean(is_active));
-
-	    Address addr = new Address();
-	    addr.setStreet(street);
-	    addr.setCity(city);
-	    addr.setZipcode(zipcode);
-	    user.setAddress(addr);
-
-	    user.setSkills(Arrays.asList(skills.split(",")));
-
+		User user = buildUser(id, email, first_name, last_name, is_active, street, city, zipcode, skills);
         given()
-            .contentType(ContentType.JSON)
+            .spec(requestSpec)
             .body(user) // Serialization happens here
         .when()
             .post("/users")
         .then()
-            .statusCode(201)
+        	.spec(responseSpec201)
             .log().ifValidationFails();
         createdIds.add(id);
     }
@@ -63,11 +72,11 @@ public class DDTexcel {
 		SoftAssert softAssert = new SoftAssert();
 		for(String id: createdIds) {
 			User user = given()
-			            .contentType(ContentType.JSON)
-			        .when()
+					.spec(requestSpec)
+					.when()
 			            .get("/users/"+id)
 			        .then()
-			            .statusCode(200)
+			            .spec(responseSpec200)
 			            .log().ifValidationFails()
 			            .extract().as(User.class);
 			
@@ -83,9 +92,11 @@ public class DDTexcel {
 		SoftAssert softAssert = new SoftAssert();
 		for(String id: createdIds) {
 			int statusCode =given()
+						.spec(requestSpec)
 				        .when()
 				            .delete("/users/"+id)
 				        .then()
+				        	.spec(responseSpec200or204)
 				            .log().ifValidationFails()
 				            .extract().statusCode();
 			softAssert.assertTrue(statusCode==200||statusCode==204, "Delete failed for ID: " + id);
@@ -98,15 +109,17 @@ public class DDTexcel {
 		SoftAssert softAssert = new SoftAssert();
 		for(String id: createdIds) {
 			String responseBody = given()
+					.spec(requestSpec)
 	        .when()
 	            .get("/users/"+id)
 	        .then()
-	            .statusCode(404)
+	            .spec(responseSpec404)
 	            .log().ifValidationFails()
 	            .extract().asString();		
 			
 			softAssert.assertEquals(responseBody.trim(), "Not Found","User ID: " + id + " was not deleted properly");
 		}
+		createdIds.clear();
 		softAssert.assertAll();
 	}
 	
@@ -116,4 +129,25 @@ public class DDTexcel {
 		String filePath = System.getProperty("user.dir") + "/src/test/resources/testData/userData.xlsx";
 	    return ExcelUtils.getExcelData(filePath, "Sheet1");
 	}
+	
+	private User buildUser(String id, String email, String first, String last,
+            String isActive, String street, String city, String zipcode,
+            String skills) {
+		User user = new User();
+		user.setId(id);
+		user.setEmail(email);
+		user.setFirst_name(first);
+		user.setLast_name(last);
+		user.setIs_active(Boolean.parseBoolean(isActive));
+		
+		Address addr = new Address();
+		addr.setStreet(street);
+		addr.setCity(city);
+		addr.setZipcode(zipcode);
+		user.setAddress(addr);
+		
+		user.setSkills(Arrays.asList(skills.split(",")));
+		return user;
+		}
+
 }
